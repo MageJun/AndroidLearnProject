42a1984ec75ab501f61c80dca968bcf66298ace7
MageJun <Lei__1991@163.com>
2017-03-07 22:59:02 +0800

    添加自定义View的包，添加BaseView

---
+++ src/com/demo/androidlearnproject/customview/BaseView.java
@@ -0,0 +1,12 @@
+package com.demo.androidlearnproject.customview;
+
+import android.content.Context;
+import android.view.View;
+
+public abstract class BaseView extends View {
+
+	public BaseView(Context context) {
+		super(context);
+	}
+
+}
